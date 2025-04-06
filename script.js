92d6537a476b13aef3af2bfc9f464177abdaf948
Nishantgit config --global user.email <nishant03937@gmail.com>
2025-04-07 00:58:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var timer = 60;
+var score =0;
+var hitrn=0;
+var clutter="";
+function makeBubble(){for(let i=1;i<=102;i++){
+    var rn=Math.floor(Math.random()*10)
+   clutter += `<div class="bubble">${rn}</div>`
+}
+document.querySelector("#pbtm").innerHTML=clutter
+}
+
+function getNewHit(){
+    hitrn  = Math.floor(Math.random()*10);
+     document.querySelector("#hitval").textContent=hitrn;
+}
+
+function runTimer(){
+    var timerInterval = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector('#timerval').textContent=timer;
+        }
+        else{
+            clearInterval(timerInterval);
+            document.querySelector("#pbtm").innerHTML=`<h1>Game Over</h1>`;
+        }
+    },1000);
+}
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent=score;
+}
+
+//Event Bubbling
+document.querySelector("#pbtm").addEventListener("click",function(details){
+    var clickNum=Number(details.target.textContent);
+    if(clickNum == hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+
+makeBubble();
+getNewHit();
+runTimer();
+// increaseScore();
+
+
